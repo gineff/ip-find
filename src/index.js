c413af4d41cb59ec61d03fbc5e2844f7141a62ec
gineff <canone@inbox.ru>
2024-09-24 13:35:07 +0300

    Динамическая работа с DOM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import { validatIp} from './helpers';
+import { validatIp } from './helpers';
 
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
+
+const ipInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timezoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
 
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
@@ -12,7 +17,7 @@ function getData() {
       `https://geo.ipify.org/api/v2/country?apiKey=at_xHMUodR2e7TIpNqUTjDnwgeFxznsh&ipAddress=${ipInput.value}`
     )
       .then((res) => res.json())
-      .then(console.log);
+      .then(setInfo);
   }
 }
 
@@ -20,4 +25,13 @@ function handleKey(e) {
   if (e.key === 'Enter') {
     getData();
   }
+}
+
+function setInfo(mapData) {
+  const { country, region, timezone } = mapData.location;
+
+  ipInfo.innerText = mapData.ip;
+  locationInfo.innerText = country + ' ' + region;
+  timezoneInfo.innerText = timezone;
+  ispInfo.innerText = mapData.isp;
 }

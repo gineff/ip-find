aff3485d509a2c38824d5a0a5b60e3d768f67997
gineff <canone@inbox.ru>
2024-09-24 13:30:41 +0300

    Валидация введенных значений

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
+import { validatIp} from './helpers';
+
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
-
 function getData() {
-  fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_xHMUodR2e7TIpNqUTjDnwgeFxznsh&ipAddress=${ipInput.value}`)
-  .then(res=> res.json())
-  .then(console.log)
+  if (validatIp(ipInput.value)) {
+    fetch(
+      `https://geo.ipify.org/api/v2/country?apiKey=at_xHMUodR2e7TIpNqUTjDnwgeFxznsh&ipAddress=${ipInput.value}`
+    )
+      .then((res) => res.json())
+      .then(console.log);
+  }
 }
 
 function handleKey(e) {
   if (e.key === 'Enter') {
-      getData();
+    getData();
   }
 }
